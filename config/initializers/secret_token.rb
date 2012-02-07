fb35b04dabf3b9b6927bb251a3e4b5893ba082e3
Xing Zhou <dekom.dev@gmail.com>
2012-02-07 09:42:38 -0500

    Initial commit with PG as DB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeroWebsite::Application.config.secret_token = '5ad10c17c4708cf21fbebb78b879e30056e2417e4048efa5e51bab24f4b2117b9aa9c171f4adcbfb0a52b5c7f9d2cf32b12bd4e54d521af8c662e15f19ae5b94'
